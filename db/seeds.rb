c1ae5ce32144fcaf9d08a91b671cfb9774895bd5
seanpierce <contact@seanpierce.us>
2017-07-07 15:50:44 -0700

    refactor db:seed 💦

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,20 @@
 Record.destroy_all
+User.destroy_all
+
+User.create!(
+  username: "Super User!",
+  email: "admin@admin.com",
+  password: "admin",
+  password_confirmation: "admin",
+  admin: true
+  )
+
+User.create!(
+  username: "Regular User",
+  email: "user@user.com",
+  password: "user",
+  password_confirmation: "user"
+  )
 
 20.times do |i|
   record = Record.create!(
@@ -11,4 +27,5 @@ Record.destroy_all
   )
 end
 
+p "Created 2 users"
 p "Created 20 records"
